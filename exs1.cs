929fb8a9f0be271069bde57c77baae29aae46b98
don-85 <muntutitus17@gmail.com>
2025-07-05 17:15:12 +0300

    updated exs1

---
+++ exs1.cs
@@ -0,0 +1,20 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string firstName = "Scott";
+        Console.WriteLine("What is your first name? " + firstName);
+    }
+    static void Main()
+     {
+        string lastName = "Burton";
+        Console.WriteLine("What is your last name? " + lastName);
+    }
+     static void Main()
+     {
+        string firstName,lastName = "Scott,Burton";
+        Console.WriteLine("Your name is " + firstName,lastName);
+    }
+}
