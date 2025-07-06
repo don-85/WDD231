a58b649d2f6c16c2cfd27e4c96f8ee46ed03cba2
don-85 <muntutitus17@gmail.com>
2025-07-06 21:43:02 +0300

    updated html

--- WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c1cd527be9b67941f1620dfd8be6c1afb547c24")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+409ac5dfe06dab2f85d92fa41fc1c5bff1bfec07")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
